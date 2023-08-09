5064b51ccb5024623ebe51eb07230330b10b1121
junu1229 <133620199+junu1229@users.noreply.github.com>
2023-08-09 14:28:52 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 3, Time: 8.02 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
@@ -1,6 +1,6 @@
 import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int[] arr, int k) {
-        return k % 2 == 0 ? IntStream.range(0, arr.length).map(i -> arr[i]+k).toArray():IntStream.range(0, arr.length).map(i -> arr[i]*k).toArray();
+        return IntStream.range(0, arr.length).map(i -> k%2==0 ? arr[i]+k : arr[i]*k ).toArray();
     }
 }
