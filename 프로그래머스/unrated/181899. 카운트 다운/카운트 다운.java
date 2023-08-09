eac46d5bbd84ae464486339bb17cbe3dd9c69211
junu1229 <133620199+junu1229@users.noreply.github.com>
2023-08-09 15:16:11 +0900

    [unrated] Title: 카운트 다운, Time: 2.07 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -1,6 +1,6 @@
 import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int start, int end) {
-        return IntStream.range(0, start-end+1).map(i -> start-i).toArray();
+        return IntStream.rangeClosed(-start, -end).map(i -> -i).toArray();
     }
 }
