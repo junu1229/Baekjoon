613b0304523a418f3444bde78315db380c909413
junu1229 <133620199+junu1229@users.noreply.github.com>
2023-08-10 17:42:18 +0900

    [unrated] Title: 배열 만들기 1, Time: 2.74 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,6 +1,6 @@
 import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int n, int k) {
-        return IntStream.range(1, (n/k)+1).map(i -> i*k).toArray();
+        return IntStream.rangeClosed(1, n/k).map(i -> i*k).toArray();
     }
 }
