d53844de645d2b842862223e74fd38b726440ff2
junu1229 <133620199+junu1229@users.noreply.github.com>
2023-08-11 16:47:42 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,6 +1,6 @@
 import java.util.stream.IntStream;
 class Solution {
     public int solution(int n) {
-        return IntStream.range(0, n).map(i->i).filter(i-> i*i==n).sum()==0? 2:1;
+        return Math.sqrt(n)%1==0? 1:2;
     }
 }
